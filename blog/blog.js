454544920e4bf2aa86ef2a6e18fdac2bec1ca4c8
ssolbae <srbae1@berkeley.edu>
2016-07-12 19:13:40 -0700

    completed likes and comments
    likes and comments done

--- blog/blog.js
+++ blog/blog.js
@@ -1,20 +1,23 @@
 var link = document.querySelector(".like-link");
 var form = document.querySelector("#new-comment");
+var like = document.querySelector(".like-count");
+var commentArea = document.querySelector("#new-comment-body")
 
-link.addEventListener("click", like);
+link.addEventListener("click", incLike);
 form.addEventListener("submit", comment);
 
-var like = document.querySelector(".like-count");
-
-function like(event) {
+function incLike(event) {
   event.preventDefault();
-
-  like.textContent += 1
   // Your code for like goes here
+  var number = parseInt(like.textContent);
+  like.textContent = number+1;
 }
 
 function comment(event) {
   event.preventDefault();
-
   // Your code for comments goes here
+  var newCommentArea = document.createElement("textarea");
+  newCommentArea.textContent = commentArea.value;
+  var body = document.querySelector("#comments");
+  body.appendChild(newCommentArea);
 }
