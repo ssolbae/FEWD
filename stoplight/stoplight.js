906127365addcc143d91296a64002a287c9be942
ssolbae <srbae1@berkeley.edu>
2016-07-21 21:03:37 -0700

    stoplight
    done?

---
+++ stoplight/stoplight.js
@@ -0,0 +1,41 @@
+var buttonStop = document.querySelector(".buttonStop");
+var buttonSlow = document.querySelector(".buttonSlow");
+var buttonGo = document.querySelector(".buttonGo");
+var buttonCaution = document.querySelector(".buttonCaution");
+
+buttonStop.addEventListener("click", turnRed);
+buttonSlow.addEventListener("click", turnYellow);
+buttonGo.addEventListener("click", turnGreen);
+buttonCaution.addEventListener("click", flickerYellow);
+
+var red = document.querySelector(".circlesStop");
+var yellow = document.querySelector(".circlesSlow");
+var green = document.querySelector(".circlesGo");
+
+function turnRed() {
+  red.classList.toggle("Stop");
+  green.clssList.remove("Go");
+  yellow.classList.remove("Slow");
+  clearInterval(intervalMagic);
+}
+function turnYellow() {
+  yellow.classList.toggle("Slow");
+  red.classList.remove("Stop");
+  green.classList.remove("Go");
+  clearInterval(intervalMagic);
+}
+function turnGreen() {
+  green.classList.toggle("Go");
+  red.classList.remove("Stop");
+  yellow.classList.remove("Slow");
+  clearInterval(intervalMagic);
+}
+function magic() {
+  yellow.classList.add("Slow")
+  red.classList.remove("Stop");
+  green.classList.remove("Go");
+}
+function flickerYellow() {
+  intervalMagic = setInterval(magic, 1000);
+
+}
--- todo-list/script.js
+++ todo-list/script.js
@@ -5,20 +5,27 @@ var body = document.querySelector("#todo-list");
 function formSubmitted(event) {
   event.preventDefault();
   // Your code goes here...
+  var todoWithoutComma = todo.value.split(",");
+  todoWithoutComma.forEach(listEach);
+
+  form.reset();
+}
+
+
+form.addEventListener("submit", formSubmitted);
+
+
+
+function listEach(ingredient) {
   var newTodo = document.createElement("li");
   var checkbox = document.createElement("input");
   var span = document.createElement("span");
   var label = document.createElement("label");
-
-  span.textContent = todo.value;
+  span.textContent = ingredient.trim();
   checkbox.setAttribute("type", "checkbox");
 
   label.appendChild(checkbox);
   label.appendChild(span);
   newTodo.appendChild(label);
   body.appendChild(newTodo);
-
-  form.reset();
 }
-
-form.addEventListener("submit", formSubmitted);
